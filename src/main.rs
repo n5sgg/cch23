6c2381591db2b83bcce4f0dc634703532263813c
Neemias Almeida <ndalmeida@bol.com>
2023-12-08 00:38:09 +0100

    feat(-1): add fake error endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use axum::{routing::get, Router};
+use axum::{
+    Router,
+    routing::get,
+    http::StatusCode
+};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn internal_error() -> Result<(), StatusCode> {
+    Err(StatusCode::INTERNAL_SERVER_ERROR)
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/error", get(internal_error));
 
     Ok(router.into())
 }
